32e80e742344ace15589bcfa7f230078ab1408fc
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-06 10:25:06 +0530

    ADDED : Function Created To Reset The Board To Start Fresh Game

--- ticTacToeSimulator.sh
+++ ticTacToeSimulator.sh
@@ -1,2 +1,28 @@
 #!/bin/bash -x
 echo "Welcome To Tic Tac Toe Game"
+
+declare -a  boardOfGame
+
+#Displays Board
+function displayBoard()
+{  
+   i=0;
+   echo "  |---|---|---| "
+   for (( counter=1 ; counter<=3 ; counter++ ))
+   do
+   echo "  | ${boardOfGame[$((counter+$i))]} | ${boardOfGame[$((counter+$i+1))]} | ${boardOfGame[$((counter+$i+2))]} | "
+   echo "  |---|---|---|"
+   i=$(($i+2))
+   done
+}
+
+#Resets Board Values 
+function resetBoard() {
+for((i=1;i<=9;i++))
+do
+   boardOfGame[$i]=$i
+done
+}
+
+resetBoard
+
