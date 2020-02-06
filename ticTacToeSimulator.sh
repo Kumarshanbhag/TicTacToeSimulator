ebd3a5471097147aaa34a0e81029b1946c37cc63
kumarshanbhag <kumarshanbhag302@gmail.com>
2020-02-06 10:50:38 +0530

    ADDED : Random Function Used To Check Who Plays First

--- ticTacToeSimulator.sh
+++ ticTacToeSimulator.sh
@@ -6,34 +6,45 @@ declare -a  boardOfGame
 #Displays Board
 function displayBoard()
 {  
-   i=0;
-   echo "  |---|---|---| "
-   for (( counter=1 ; counter<=3 ; counter++ ))
-   do
-   echo "  | ${boardOfGame[$((counter+$i))]} | ${boardOfGame[$((counter+$i+1))]} | ${boardOfGame[$((counter+$i+2))]} | "
-   echo "  |---|---|---|"
-   i=$(($i+2))
-   done
+	i=0;
+	echo "  |---|---|---| "
+	for (( counter=1 ; counter<=3 ; counter++ ))
+	do
+		echo "  | ${boardOfGame[$((counter+$i))]} | ${boardOfGame[$((counter+$i+1))]} | ${boardOfGame[$((counter+$i+2))]} | "
+		echo "  |---|---|---|"
+		i=$(($i+2))
+	done
 }
 
 #Resets Board Values 
 function resetBoard() {
 	for((i=1;i<=9;i++))
 	do
-   	boardOfGame[$i]=$i
+		boardOfGame[$i]=$i
 	done
 }
 
-#Player  Gets Letter Assigned X or O
+#Player Gets Letter Assigned X or O
 function playerLetter()
 {
-   if((RANDOM%2==1))
-   then
-		readonly PLAYER=X
-   else
-      readonly PLAYER=O
-   fi
-   echo "Player Letter = $PLAYER"
+	if((RANDOM%2==1))
+	then
+		PLAYER=X
+	else
+		PLAYER=O
+	fi
+	echo "Player Letter = $PLAYER"
+	firstChance
+}
+
+#Toss to check who plays first
+function firstChance() {
+	if((RANDOM%2==1))
+	then
+		echo "Player First Turn"
+	else
+		echo "Player First Turn"
+	fi
 }
 
 resetBoard
